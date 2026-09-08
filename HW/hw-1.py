53073ef5b9d4cada4580de92d8538cff29b9c1eb
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-09-08 16:00:41 -0400

    hw1 mean, median

--- HW/hw-1.py
+++ HW/hw-1.py
@@ -1,19 +1,26 @@
-import pandas as pd
+import numpy
+# from scipy import stats
 
-# 1
-list1 = {"speeds": [75, 100, 95, 120, 45, 75, 45, 95, 75]}
+# 1 - mode
+# list1 = {"speeds": [75, 100, 95, 120, 45, 75, 45, 95, 75]}
+# mode1 = stats.mode(list1["speeds"])
+# print("mode: ", mode1.mode)
 
-# 2
+# # 2 - mean
 list2 = {"ages": [23, 21, 25, 24, 20]}
+mean2 = numpy.mean(list2["ages"])
+print("mean: ", mean2)
 
-# 3
+# 3 - median
 list3 = {"megabytes": [110, 90, 200, 250, 280]}
+med3 = numpy.median(list3["megabytes"])
+print("median: ", med3)
 
-# 4
+# 4 - no median, no mode, no mean
 # list4 = {"": []}
 
-# 5
+# 5 - total spent
 # list5 = {"": []}
 
-# 6
+# 6 - variance and standard deviation
 list6 = {"spam_emails": [20, 10, 25, 35, 50]}
