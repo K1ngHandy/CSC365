8cee352b7c503678ed97799478758f08c32fe183
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-09-08 16:21:31 -0400

    import scipy

--- HW/hw-1.py
+++ HW/hw-1.py
@@ -1,20 +1,23 @@
 import numpy
-# from scipy import stats
+from scipy import stats
 
 # 1 - mode
-# list1 = {"speeds": [75, 100, 95, 120, 45, 75, 45, 95, 75]}
-# mode1 = stats.mode(list1["speeds"])
-# print("mode: ", mode1.mode)
+list1 = {"speeds": [75, 100, 95, 120, 45, 75, 45, 95, 75]}
+mode1 = stats.mode(list1["speeds"], keepdims=False)
+
+print("Mode: ", mode1.mode)
 
 # # 2 - mean
 list2 = {"ages": [23, 21, 25, 24, 20]}
 mean2 = numpy.mean(list2["ages"])
-print("mean: ", mean2)
+
+print("Mean: ", mean2)
 
 # 3 - median
 list3 = {"megabytes": [110, 90, 200, 250, 280]}
 med3 = numpy.median(list3["megabytes"])
-print("median: ", med3)
+
+print("Median: ", med3)
 
 # 4 - no median, no mode, no mean
 # list4 = {"": []}
@@ -24,3 +27,11 @@ print("median: ", med3)
 
 # 6 - variance and standard deviation
 list6 = {"spam_emails": [20, 10, 25, 35, 50]}
+A={"A": [10,2,3,4,5,6,7,8,9,10]}
+
+variance = numpy.var(list6["spam_emails"])
+standard = numpy.std(list6["spam_emails"])
+
+print("")
+print("Variance: ", variance)
+print("Standard Dev: ", standard)
