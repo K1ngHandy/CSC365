d7f2e9b0a6700d2439bee6eab8d6a9e4a1047583
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-09-08 16:30:27 -0400

    add sample data, total

--- HW/hw-1.py
+++ HW/hw-1.py
@@ -20,10 +20,14 @@ med3 = numpy.median(list3["megabytes"])
 print("Median: ", med3)
 
 # 4 - no median, no mode, no mean
-# list4 = {"": []}
+list4 = {"sample": []}
+print("Sample: ", list4["sample"])
 
 # 5 - total spent
-# list5 = {"": []}
+avg = 200 # $200
+persons = 10
+total = avg * persons
+print("Total: ", total)
 
 # 6 - variance and standard deviation
 list6 = {"spam_emails": [20, 10, 25, 35, 50]}
@@ -32,6 +36,5 @@ A={"A": [10,2,3,4,5,6,7,8,9,10]}
 variance = numpy.var(list6["spam_emails"])
 standard = numpy.std(list6["spam_emails"])
 
-print("")
 print("Variance: ", variance)
 print("Standard Dev: ", standard)
