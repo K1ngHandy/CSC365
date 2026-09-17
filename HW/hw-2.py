f75007e7573c1ad92363290411bedd5a2345787a
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-09-17 02:04:44 -0400

    add probability calculation

--- HW/hw-2.py
+++ HW/hw-2.py
@@ -12,13 +12,27 @@ File = pandas.read_csv('HW/dataset_2.csv')
 File = File.dropna(axis = 1, how = 'all')
 
 # 1
-# print(File)
+print(File)
 
 # 2
 print("Total Customers: ", File.shape[0])
 
 # 3
+# P(Purchase = Yes | Income = High)
+high_income = File[File['income_level'].str.lower() == 'high']
 
+purchased = (
+    high_income['made_purchase']
+    .str.lower()
+    .eq('yes')
+    .sum()
+)
+
+total_high = len(high_income)
+prob = purchased / total_high
+
+print(f"Probability: {purchased} / {total_high} = {prob}")
+print(f"Percentage: {prob * 100:.2f}%")
 
 # 4
 frequency_count = File['visit_frequency'].value_counts()
