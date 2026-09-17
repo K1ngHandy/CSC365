07dc2c810707b06d8139b8a436f62d1112e32829
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-09-16 21:12:50 -0400

    print data, num of cust

--- HW/hw-2.py
+++ HW/hw-2.py
@@ -2,3 +2,11 @@
 # Stephen Handy
 # 0796861
 
+# dataset
+import pandas
+File = pandas.read_csv('HW/dataset_2.csv')
+File = File.dropna(axis = 1, how = 'all')
+
+print(File)
+
+print("Number of Customers: ", File.shape[0])
