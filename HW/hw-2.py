e13db670b63ed12498cdd90ff7ae8d7198a8ac9e
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-09-16 21:36:03 -0400

    move bar chart to correct number

--- HW/hw-2.py
+++ HW/hw-2.py
@@ -2,11 +2,32 @@
 # Stephen Handy
 # 0796861
 
-# dataset
+# imports
 import pandas
+import matplotlib.pyplot as plt
+import numpy as np
+
+# dataset
 File = pandas.read_csv('HW/dataset_2.csv')
 File = File.dropna(axis = 1, how = 'all')
 
-print(File)
+# 1
+# print(File)
 
-print("Number of Customers: ", File.shape[0])
+# 2
+print("Total Customers: ", File.shape[0])
+
+# 3
+
+
+# 4
+frequency_count = File['visit_frequency'].value_counts()
+
+plt.figure(figsize=(8, 5))
+plt.barh(frequency_count.index, frequency_count.values)
+
+plt.xlabel('Number of Purchases')
+plt.ylabel('Visit Frequency')
+plt.title('Frequency of Visits to Purchases Made')
+
+plt.show()
